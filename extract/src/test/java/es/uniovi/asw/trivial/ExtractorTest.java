7c3c9509d56d282b479123834ad3db5681dc537f
unknown <robertene1994@gmail.com>
2015-03-05 02:09:10 +0100

    Retoques Casi Finales Aplicacion

--- extract/src/test/java/es/uniovi/asw/trivial/ExtractorTest.java
+++ extract/src/test/java/es/uniovi/asw/trivial/ExtractorTest.java
@@ -1,14 +1,166 @@
 package es.uniovi.asw.trivial;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import es.uniovi.asw.trivial.app.GIFTParser;
+import es.uniovi.asw.trivial.app.MongoParser;
+import es.uniovi.asw.trivial.app.TrivialApp;
+import es.uniovi.asw.trivial.model.Answer;
+import es.uniovi.asw.trivial.model.Question;
+import es.uniovi.asw.trivial.model.Trivial;
+import es.uniovi.asw.trivial.persistence.Factory;
+import es.uniovi.asw.trivial.persistence.TrivialDAO;
 
 public class ExtractorTest {
 
+	final String[] realQuestions = { 	"¿Quién 'sabía que no sabía nada'?",
+										"¿En qué lugar del cuerpo se produce la insulina?",
+										"¿En que ciudad nacio Fernando alonso?",
+										"¿Con qué se fabricaba el pergamino?",
+										"¿Qué atleta se hizo famoso por utilizar una nueva técnica en atletismo?" };
+	
+	final String[] realAnswers = { 	"Aristóteles", "Ortega y Gasset", "Sócrates",
+									"En la hipófisis", "En el páncreas", "En el duodeno",
+									"Oviedo", "Londres", "Gijon",
+									"Con piel de animales", "Con tiras de madera", "Con hojas de arbusto",
+									"Dick Fosbury", "Carl Lewis","Emil Zátopek" };
+	
+	final String[] realCorrectAnswers = { 	"Sócrates", "En el páncreas","Oviedo","Con hojas de arbusto","Dick Fosbury" };
+	final String[] realCategories = { "", "", "", "", "" };
+	final String[] realComments = { "", "", "", "", "" };
+	
+	
 	@Test
-	public void emptyExtractor() {
-	   assert(false);
-		// String args[] = {};
-		// Extractor ext = new Extractor();
-		// assertThat(ext.run(args)).isEqualTo(0);
+	public void testParserGift() {
+		
+		try {
+		
+			TrivialApp app = new TrivialApp(new GIFTParser("test1.gift"));
+			Trivial trivial = app.getTrivial();
+			Assert.assertNotNull(trivial);
+			List<Question> questions = trivial.getQuestions();
+			
+			for (int i=0, j = 0; i< questions.size(); i++){
+				Question preg = questions.get(i);
+				
+				Assert.assertTrue(preg.getName().equals("Pregunta " + (i + 1)));
+				Assert.assertTrue(preg.getQuestion().equals(realQuestions[i]));
+				
+				for (Answer answer : preg.getAnswers()) {
+					
+					Assert.assertTrue(answer.getAnswer().equals(realAnswers[j]));
+					
+					if( answer.getAnswer().equals(realCorrectAnswers[i]) ){
+						Assert.assertTrue(answer.getIsCorrect());
+					}
+					
+					j++;
+				}
+				
+				
+			//	Assert.assertTrue(preg.getCategory().equals(realCategories[i]));
+			//	Assert.assertTrue(preg.getComments().equals(realComments[i]));
+				
+			}
+
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
+	
+	
+	@Test
+	public void testLeerDB() {
+		
+		TrivialDAO trivialDAO = Factory.persistence.createTrivialDAO();
+		
+		try {
+			
+			trivialDAO.deleteAllQuestions();
+			
+			TrivialApp app = new TrivialApp(new GIFTParser("test1.gift"));
+			Assert.assertNotNull(app);
+			
+			trivialDAO.saveQuestions(app.getTrivial());
+			
+			app = new TrivialApp(new MongoParser());
+			
+			Trivial trivial = app.getTrivial();
+			Assert.assertNotNull(trivial);
+			List<Question> questions = trivial.getQuestions();
+			
+			for (int i = 0, j=0; i < questions.size(); i++) {
+				Question preg = questions.get(i);
+
+				Assert.assertTrue(preg.getName().equals("Pregunta " + (i + 1)));
+				Assert.assertTrue(preg.getQuestion().equals(realQuestions[i]));
+				
+				for (Answer answer : preg.getAnswers()) {
+					
+					Assert.assertTrue(answer.getAnswer().equals(realAnswers[j]));
+					
+					if( answer.getAnswer().equals(realCorrectAnswers[i]) ){
+						Assert.assertTrue(answer.getIsCorrect());
+					}
+					
+					j++;
+				}
+			//	Assert.assertTrue(preg.getCategory().equals(realCategories[i]));
+			//	Assert.assertTrue(preg.getComments().equals(realComments[i]));
+				
+			}
+			
+		} catch (IOException e) {
+			Assert.assertTrue(false);
+		}
+		
+		
+		
+		Assert.assertFalse(trivialDAO.findAllQuestions().isEmpty());
+		
+		trivialDAO.deleteAllQuestions();
+		
+		Assert.assertTrue(trivialDAO.findAllQuestions().isEmpty());
+		
+	}
+	
+	
+
+	@Test
+	public void testArchivoDesconocido() {
+		
+		try {
+		
+			TrivialApp app = new TrivialApp(new GIFTParser("asf.gift"));
+			Trivial trivial = app.getTrivial();
+			Assert.assertNull(trivial);
+			
+			Assert.assertTrue(false);
+		} catch (IOException e) {
+			Assert.assertTrue(true);
+		}
+		
+	}
+	
+	@Test
+	public void testFormatoDesconocido() {
+		
+		try {
+			TrivialApp app = new TrivialApp(new GIFTParser("test2.gift"));
+			
+		} catch (IOException e) {
+			Assert.assertTrue(false);
+		}
+
+		
+	}
+	
+	
+	
 }
