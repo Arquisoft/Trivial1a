a90126a407ee6e29c4a845d2def934056366e7eb
Francisco Javier Gil Gala <fjgilgala@gmail.com>
2015-04-07 16:17:34 +0200

    Añadidos JavaDoc y test
    Añadidos test y JavaDoc al tablero. Los test están hechos de forma
    rápida pero prueba todo el funcionamiento del tablero…

--- game/src/main/java/es/uniovi/asw/game/model/Tablero.java
+++ game/src/main/java/es/uniovi/asw/game/model/Tablero.java
@@ -12,7 +12,7 @@ public class Tablero {
 		this.nPasillos = nPasillos;
 		this.tArco = tArco;
 		this.tPasillo = tPasillo;
-		tamaño = nArcos * tArco + nPasillos * this.tPasillo + nArcos +1; // incluyendo la casilla centro
+		tamaño = nArcos * tArco + nPasillos * this.tPasillo + nArcos +1; // incluyendo la casilla centro (la 0)
 	}
 	
 	/**
@@ -26,31 +26,45 @@ public class Tablero {
 		if (n == 0)
 			return null; // fin del juego
 		else if (n >= 1 && n <= nArcos * tArco && posibleQuesito(n)) // si la casilla es un quesito
-			if (n == 1){
-				int [] posiciones = {n + 1, nArcos * tArco + tArco, calculaPosicionQuesito(n)};
-				return posiciones; 
-			} else{
-				int [] posiciones = {n + 1, n -1, calculaPosicionQuesito(n)};
-				return posiciones;
-			}
-		else if (n > nArcos * tArco + tArco && n < tamaño - 1 && posibleFin(n)){ // si la casilla es la anterior al final
-			int [] posiciones = {0, n -1}; // la siguiente será la casilla 0
-			return posiciones;
-		}
+			if (n == 1)
+				return new int []{n + 1, nArcos * tArco + tArco, calculaPosicionQuesito(n)};
+			else
+				return new int []{n + 1, n -1, calculaPosicionQuesito(n)};
+		else if (n > nArcos * tArco + tArco && n < tamaño - 1 && posibleFin(n)) // si la casilla es la anterior al final
+			return new int []{0, n -1}; // la siguiente será la casilla 0
 		else
-			return null;
+			if (n>= 0 && n <= tamaño -1)
+				return new int []{n+1, n-1};
+			else 
+				return null;
 	}
 	
-	private int calculaPosicionQuesito(int n){
-		return (tamaño - nArcos*tArco) - 2 * ((int) n / (tArco +1)) + n;
+	/**
+	 * Calcula la primera posición en la calle saliendo de un quesito
+	 * @param n
+	 * @return int
+	 */
+	public int calculaPosicionQuesito(int n)
+	{
+		return (tamaño - nPasillos*tPasillo) - 2 * ((int) n / (tArco +1)) + n -1;
 	}
 	
-	private boolean posibleQuesito(int n)
+	/**
+	 * Indica si la casilla pertenece a un quesito
+	 * @param n
+	 * @return
+	 */
+	public boolean posibleQuesito(int n)
 	{
 		return n % (tArco + 1) - 1 == 0;
 	}
 	
-	private boolean posibleFin(int n)
+	/**
+	 * Indica si la siguiente casilla será la 0
+	 * @param n
+	 * @return
+	 */
+	public boolean posibleFin(int n)
 	{
 		return n % tPasillo - 2 == 0;
 	}
---
+++ game/src/test/java/es/uniovi/asw/trivial/TestTablero.java
@@ -0,0 +1,57 @@
+package es.uniovi.asw.trivial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import es.uniovi.asw.game.model.Tablero;
+
+public class TestTablero {
+
+	@Test
+	public void test() {
+		Tablero t = new Tablero(6,6,6,5);
+		
+		assertFalse(t.posibleQuesito(0));
+		assertFalse(t.posibleQuesito(42));
+		assertFalse(t.posibleQuesito(2));
+		assertTrue(t.posibleQuesito(1));
+		assertTrue(t.posibleQuesito(8));
+		assertTrue(t.posibleQuesito(15));
+		assertTrue(t.posibleQuesito(22));
+		assertTrue(t.posibleQuesito(29));
+		assertTrue(t.posibleQuesito(36));
+		
+		assertFalse(t.posibleFin(0));
+		assertFalse(t.posibleFin(1));
+		assertTrue(t.posibleFin(47));
+		assertTrue(t.posibleFin(52));
+		assertTrue(t.posibleFin(57));
+		assertTrue(t.posibleFin(62));
+		assertTrue(t.posibleFin(67));
+		assertTrue(t.posibleFin(72));
+
+		assertEquals(43, t.calculaPosicionQuesito(1));
+		assertEquals(48, t.calculaPosicionQuesito(8));
+		assertEquals(53, t.calculaPosicionQuesito(15));
+		assertEquals(58, t.calculaPosicionQuesito(22));
+		assertEquals(63, t.calculaPosicionQuesito(29));
+		assertEquals(68, t.calculaPosicionQuesito(36));
+
+
+		assertArrayEquals(new int[]{2,42,43}, t.calculaSiguienteCasilla(1));
+		assertArrayEquals(new int[]{9,7,48}, t.calculaSiguienteCasilla(8));
+		assertArrayEquals(new int[]{16,14,53}, t.calculaSiguienteCasilla(15));
+		assertArrayEquals(new int[]{23,21,58}, t.calculaSiguienteCasilla(22));
+		assertArrayEquals(new int[]{30,28,63}, t.calculaSiguienteCasilla(29));
+		assertArrayEquals(new int[]{37,35,68}, t.calculaSiguienteCasilla(36));
+		
+		assertArrayEquals(new int[]{3,1}, t.calculaSiguienteCasilla(2));
+		assertArrayEquals(new int[]{71,69}, t.calculaSiguienteCasilla(70));
+		assertArrayEquals(null, t.calculaSiguienteCasilla(78));
+		assertArrayEquals(null, t.calculaSiguienteCasilla(0));
+
+
+	}
+
+}
